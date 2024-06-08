e3d80a1c987a26d734c73ea8bf88828482713e69
Jan Jaap van Laar <691510@student.inholland.nl>
2024-06-08 13:15:53 +0200

    Update RandomForestChess.py

--- RandomForestChess.py
+++ RandomForestChess.py
@@ -13,7 +13,6 @@ except ImportError:
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import joblib
 
